525b2af15907c7d2cb0c2c0791648cbdf6a13453
Andrei Novitski <anjeynov@icloud.com>
2024-03-11 20:47:26 +0100

    some

--- Sources/Voiper/Service/Account/CallManager.swift
+++ Sources/Voiper/Service/Account/CallManager.swift
@@ -109,9 +109,6 @@ public class CallManager: NSObject {
                         serverConfiguration: CallManager.txServerConfig
                     )
                 telnyxClient?.delegate = self
-                DispatchQueue.main.asyncAfter(deadline: .now() + 5) { [telnyxClient] in
-                    telnyxClient?.disconnect()
-                }
                 seal.fulfill(())
             } catch {
                 Crashlytics.crashlytics().record(error: error)
